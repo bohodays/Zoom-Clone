b60d21f1fb15a04353c9cfdf50a57653233f91db
bohodays <bohodays0316@gmail.com>
2023-01-06 17:38:07 +0900

    Update: day02

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from 'http';
+import WebSocket from 'ws';
 import express from 'express';
 
 const app = express();
@@ -9,4 +11,25 @@ app.get('/', (req, res) => res.render('home'));
 app.get('/*', (req, res) => res.redirect('/'));
 
 const handleListen = () => console.log(`Listen on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+wss.on('connection', (socket) => {
+  sockets.push(socket);
+  console.log("Connected to Brower");
+  socket.on('close', () => {
+    console.log("브라우저 연결 끊김");
+  })
+  socket.on('message', (message) => {
+    sockets.forEach((aScocket) => {
+      aScocket.send(message.toString());
+    })
+  });
+  socket.send("hello!!!");
+})
+
+server.listen(3000, handleListen);
+
